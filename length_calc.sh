5e233434aa0c00e80ccfc0ada8c8bbc764d38f29
Jorge Donis <jdda2@alu.ua.es>
2020-03-05 15:13:10 +0100

    length_calc calcula el tamaño de los ficheros de salida

---
+++ length_calc.sh
@@ -0,0 +1,20 @@
+#!/bin/bash 
+
+# cp ./src/test_speed.cpp ./src/main.cpp
+# make
+# ./practica1
+
+
+for file in corpus/*
+do
+    if [[ $file == *".txt" ]]; then
+        file_size=$(ls -l  $file | cut -d " " -f5)
+        token_size=$(ls -l $file.tk | cut -d " " -f5)
+        if [ "$file_size" -ge "$token_size" ]; then
+            pctg=`echo "(($file_size - $token_size) / $file_size) * 100" | bc -l`
+            echo Tamaño de $file = [$file_size] vs [$token_size] $pctg
+        fi
+    fi
+done
+
+# rm -f corpus/*.tk
