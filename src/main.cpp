e57b721d1d4e1bb0426a442665764461e3206b81
Jorge Donis <jdda2@alu.ua.es>
2020-03-07 22:30:29 +0100

    tarda 0.53, creo que los ficheros bien

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,23 @@
 #include "tokenizador.h"
 using namespace std;
 
+double getcputime(void)
+{
+        struct timeval tim;
+        struct rusage ru;
+        getrusage(RUSAGE_SELF, &ru);
+        tim = ru.ru_utime;
+        double t = (double)tim.tv_sec + (double)tim.tv_usec / 1000000.0;
+        tim = ru.ru_stime;
+        t += (double)tim.tv_sec + (double)tim.tv_usec / 1000000.0;
+        return t;
+}
 
 int main()
 {
-        Tokenizador a("\t ,;:.-+/*_`'{}[]()!?&#\"\\<>\n@", true, true);
-        a.Tokenizar("corpus.txt");
+        long double aa = getcputime();
+        Tokenizador a("\t ,;:.-+/*_`'{}[]()!?&#\"\\<>", true, true);
+        a.TokenizarListaFicheros(".lista_ficheros");
+        cout << "Ha tardado " << getcputime() - aa << " segundos" << endl;
         return 0;
 }
