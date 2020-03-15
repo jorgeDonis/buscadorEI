f014aa91cea1b5d1006d4250747f8829d7ed4877
Jorge Donis del Álamo <jorge@debian.villamar>
2020-03-15 10:30:10 +0100

    clases de información (sin tests)

--- include/indexadorInformacion.h
+++ include/indexadorInformacion.h
@@ -4,49 +4,97 @@
 #include <iostream>
 #include <unordered_map>
 #include <list>
+#include <unordered_map>
 
 class InfTermDoc
 {
     friend std::ostream& operator<<(std::ostream& s, const InfTermDoc& p);
-
-    public:
-        InfTermDoc(const InfTermDoc &);
-        InfTermDoc();
-        // Inicializa ft = 0
-        ~InfTermDoc();
-        // Pone ft = 0
-        InfTermDoc &operator=(const InfTermDoc &);
-        // Añadir cuantos métodos se consideren necesarios para manejar la parte
-        //privada de la clase private : int ft;
-        // Frecuencia del término en el documento
+    private:
+        int ft;
         std::list<int> posTerm;
-        // Solo se almacenará esta información si el campo privado del indexador
-        //almacenarPosTerm == true
-        //                    // Lista de números de palabra en los que aparece el término en el
-        //                    documento.Los números de palabra comenzarán desde
-        //                    cero(la primera
-        //                             palabra del documento)
-        //                        .Se numerarán las palabras de parada.Estará
-        //                    ordenada de menor a mayor posición.
+        void copy_vals(const InfTermDoc&);
+    public:
+        InfTermDoc() {ft = 0;}
+        InfTermDoc(const InfTermDoc &);
+        ~InfTermDoc();
+        InfTermDoc& operator=(const InfTermDoc&);
 };
 
 class InformacionTermino
 {
     friend std::ostream& operator<<(std::ostream& s, const InformacionTermino& p);
-    public:
-        InformacionTermino(const InformacionTermino &);
-        InformacionTermino();
-        // Inicializa ftc = 0
-        ~InformacionTermino();
-        // Pone ftc = 0 y vacía l_docs
-        InformacionTermino &operator=(const InformacionTermino &);
-        // Añadir cuantos métodos se consideren necesarios para manejar la parte
-        // privada de la clase private : int ftc; // Frecuencia total del término en la colección
+    private:
+        int ftc;
         std::unordered_map<long int, InfTermDoc> l_docs;
-        // Tabla Hash que se accederá por el id del documento, devolviendo un
-        //objeto de la clase InfTermDoc que contiene toda la información de
-        //    aparición del término en el documento
+        void copy_vals(const InformacionTermino&);
+    public:
+        InformacionTermino() {ftc = 0;}
+        InformacionTermino(const InformacionTermino&);
+        ~InformacionTermino();
+        InformacionTermino& operator=(const InformacionTermino&);
 };
 
+class InfDoc
+{
+    static long int DOC_ID;
+    friend std::ostream& operator<<(std::ostream&, const InfDoc&);
+    private:
+        long int idDoc;
+        int numPal;
+        int numPalSinParada;
+        int numPalDiferentes;
+        int tamBytes;
+        time_t fechaModificacion;
+        void copy_vals(const InfDoc&);
+    public:
+        InfDoc();
+        InfDoc(const InfDoc&);
+        ~InfDoc() {InfDoc::DOC_ID--;}
+        InfDoc& operator=(const InfDoc&);
+};
+
+class InfColeccionDocs
+{
+    friend std::ostream& operator<<(std::ostream&, const InfColeccionDocs&);
+    private:
+        long int numDocs;
+        long int numTotalPal;
+        long int numTotalPalSinParada;
+        long int numTotalPalDiferentes;
+        long int tamBytes;
+        void copy_vals(const InfColeccionDocs&);
+    public:
+        InfColeccionDocs();
+        InfColeccionDocs(const InfColeccionDocs&);
+        InfColeccionDocs& operator=(const InfColeccionDocs&);
+};
+
+class InformacionTerminoPregunta
+{
+    friend std::ostream& operator<<(std::ostream&, const InformacionTerminoPregunta&);
+    private:
+        int ft;
+        std::list <int> posTerm;
+        void copy_vals(const InformacionTerminoPregunta&);
+    public:
+        InformacionTerminoPregunta() {ft = 0;}
+        InformacionTerminoPregunta(const InformacionTerminoPregunta& foo) {copy_vals(foo);}
+        ~InformacionTerminoPregunta() {posTerm.clear();}
+        InformacionTerminoPregunta& operator=(const InformacionTerminoPregunta&);
+};
+
+class InformacionPregunta
+{
+    friend std::ostream& operator<<(std::ostream&, const InformacionPregunta&);
+    private:
+        long int numTotalPal;
+        long int numTotalPalSinParada;
+        long int numTotalPalDiferentes;
+        void copy_vals(const InformacionPregunta&);
+    public:
+        InformacionPregunta() {numTotalPal = numTotalPalSinParada = numTotalPalDiferentes = 0;}
+        InformacionPregunta(const InformacionPregunta& foo) {copy_vals(foo);}
+        InformacionPregunta& operator=(const InformacionPregunta&);
+};
 
 #endif
--- lib/indexadorInformacion.cpp
+++ lib/indexadorInformacion.cpp
@@ -1,1 +1,198 @@
-#include "indexadorInformacion.h" 
+#include "indexadorInformacion.h"
+#include <ctime>
+
+long int InfDoc::DOC_ID = 0;
+
+using namespace std;
+
+ostream& operator<<(ostream& s, const InfTermDoc& p)
+{
+    s << "ft: " << p.ft;
+    for (const int pos : p.posTerm)
+        s << pos << "\t";
+    s << "\b";
+    return s;
+}
+
+void InfTermDoc::copy_vals(const InfTermDoc& foo)
+{
+    this->ft = foo.ft;
+    for (int pos : foo.posTerm)
+        this->posTerm.push_back(pos);
+}
+
+InfTermDoc::InfTermDoc(const InfTermDoc& foo)
+{
+    copy_vals(foo);
+}
+
+InfTermDoc& InfTermDoc::operator=(const InfTermDoc& foo)
+{
+    if (&foo != this)
+    {
+        this->~InfTermDoc();
+        copy_vals(foo);
+    }
+    return *this;
+}
+
+InfTermDoc::~InfTermDoc()
+{
+    posTerm.clear();
+}
+
+ostream& operator<<(ostream& os, const InformacionTermino& it)
+{
+    os << "Frecuencia total: " << it.ftc << "\tfd: " << it.l_docs.size();
+    for (const pair<long int, InfTermDoc>& entry : it.l_docs)
+        os << "\tId.Doc: " << entry.first << "\t" << entry.second;
+    return os;
+}
+
+void InformacionTermino::copy_vals(const InformacionTermino& foo)
+{
+    this->ftc = foo.ftc;
+    for (const pair<long int, InfTermDoc>& entry : foo.l_docs)
+        this->l_docs.insert(entry);
+}
+
+InformacionTermino::InformacionTermino(const InformacionTermino& foo)
+{
+    copy_vals(foo);
+}
+
+InformacionTermino::~InformacionTermino()
+{
+    l_docs.clear();
+}
+
+InformacionTermino& InformacionTermino::operator=(const InformacionTermino& foo)
+{
+    if (this != &foo)
+    {
+        this->~InformacionTermino();
+        copy_vals(foo);
+    }
+    return *this;
+}
+
+ostream& operator<<(ostream& os, const InfDoc& id)
+{
+    os << "idDoc: " << id.idDoc << "\tnumPal: " << id.numPal << "\tnumPalSinParada: "
+    << id.numPalSinParada << "\tnumPalDiferentes: " << id.numPalDiferentes << "\ttamBytes: "
+    << id.tamBytes;
+    return os;
+}
+
+void InfDoc::copy_vals(const InfDoc& foo)
+{
+    this->fechaModificacion = foo.fechaModificacion;
+    this->idDoc = foo.idDoc;
+    this->numPal = foo.numPal;
+    this->numPalDiferentes = numPalDiferentes;
+    this->numPalSinParada = numPalSinParada;
+    this->tamBytes = foo.tamBytes;
+}
+
+InfDoc::InfDoc()
+{
+    idDoc = InfDoc::DOC_ID++;
+    fechaModificacion = time(NULL);
+    numPal = numPalDiferentes = numPalSinParada = tamBytes = 0;
+}
+
+InfDoc::InfDoc(const InfDoc& foo)
+{
+    copy_vals(foo);
+}
+
+InfDoc& InfDoc::operator=(const InfDoc& foo)
+{
+    if (this != &foo)
+    {
+        this->~InfDoc();
+        copy_vals(foo);
+    }
+    return *this;
+}
+
+ostream& operator<<(ostream& os, const InfColeccionDocs& icd)
+{
+    os << "numDocs: " << icd.numDocs << "\tnumTotalPal: " << icd.numTotalPal << "\tnumTotalPalSinParada: " <<
+    icd.numTotalPalSinParada << "\tnumTotalPalDiferentes: " << icd.numTotalPalDiferentes << "\ttamBytes" <<
+    icd.tamBytes;
+    return os;
+}
+
+InfColeccionDocs::InfColeccionDocs()
+{
+    numDocs = numTotalPal = numTotalPalSinParada = numTotalPalDiferentes = tamBytes = 0;
+}
+
+void InfColeccionDocs::copy_vals(const InfColeccionDocs& foo)
+{
+    numDocs = foo.numDocs;
+    numTotalPal = foo.numTotalPal;
+    numTotalPalSinParada = foo.numTotalPalSinParada;
+    numTotalPalDiferentes = foo.numTotalPalDiferentes;
+    tamBytes = foo.tamBytes;
+}
+
+InfColeccionDocs::InfColeccionDocs(const InfColeccionDocs& foo)
+{
+    copy_vals(foo);
+}
+
+InfColeccionDocs& InfColeccionDocs::operator=(const InfColeccionDocs& foo)
+{
+    if (&foo != this)
+        copy_vals(foo);
+    return *this;
+}
+
+ostream& operator<<(ostream& os, const InformacionTerminoPregunta& itp)
+{
+    os << "ft: " << itp.ft;
+    for (const int pos : itp.posTerm)
+        os << pos << "\t";
+    os << "\b";
+    return os;
+}
+
+void InformacionTerminoPregunta::copy_vals(const InformacionTerminoPregunta& foo)
+{
+    ft = foo.ft;
+    for (const int pos : foo.posTerm)
+        posTerm.push_back(pos);
+}
+
+InformacionTerminoPregunta& InformacionTerminoPregunta::operator=(const InformacionTerminoPregunta& foo)
+{
+    if (&foo != this)
+    {
+        this->~InformacionTerminoPregunta();
+        copy_vals(foo);
+    }
+    return *this;
+}
+
+ostream& operator<<(ostream& os, const InformacionPregunta& ip)
+{
+    os << "numTotalPal: " << ip.numTotalPal << "\tnumTotalPalSinParada: " << ip.numTotalPalSinParada <<
+    "\numTotalPalDiferentes: " << ip.numTotalPalDiferentes;
+    return os;
+}
+
+void InformacionPregunta::copy_vals(const InformacionPregunta& foo)
+{
+    numTotalPal = foo.numTotalPal;
+    numTotalPalSinParada = foo.numTotalPalSinParada;
+    numTotalPalDiferentes = foo.numTotalPalDiferentes;
+}
+
+InformacionPregunta& InformacionPregunta::operator=(const InformacionPregunta& foo)
+{
+    if (&foo != this)
+        copy_vals(foo);
+    return *this;
+}
